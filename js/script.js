d1c1e5b858d9331c990dd3c8d915fed452cacad5
InnaCherny22 <112832558+InnaCherny22@users.noreply.github.com>
2022-11-27 15:35:34 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+"use strict"
+console.log("Я учу JS");
+console.log('JS');
+console.log('Учим');
+let myLifeStyle = 'freelance';
+console.log(myLifeStyle);
+let myName = 'Inna',
+    myAge = 38,
+	myMessage = 'it all depends on you!';
+myAge = 22;
+console.log(myAge);
+const BLOCK_SIZE = 25;
+const COLOR_RED = "#F00";
+const summSizes = 25 + 30;
+const userProfile = {
+	name: 'Inna',
+	age: 38,
+	message: 'it all depends on you!'
+}
+console.log(userProfile);
+userProfile.age = 22;
+console.log(userProfile);
+// Домашка (переменные) завдання №1
+let userEyeColor;
+// Домашка (переменные) завдання №2
+let user;
+let userName;
+userName = 'Вася';
+user = userName;
+console.log(user);
+const BLOCK_HEIGHT = 100;
+console.log(BLOCK_HEIGHT);
+if (true) {
+	var size = 15;
+}
+console.log(size);
+
+let userAge = 36;
+let userAgeInfo = `Фрилансер: ${userAge}`;
+
+
+let userHeight = 145 / 0;
+console.log(userHeight);
+
+let userView;
+console.log(typeof userViev);
+
+let userSize = "45" / "8";
+console.log(typeof userSize);
